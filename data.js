5401fb0d645b8f4ae984ab84e4ee97620761e61e
TUSHAR-SHUKLA <shukla.tushar.456@gmail.com>
2024-06-13 14:16:35 +0530

    KanBan App

---
+++ data.js
@@ -0,0 +1,42 @@
+//-------- This file is about the data Structure our Kanban App has implemented!! ---- //
+
+// export const data=[
+//     {
+//         "columnId":0,
+//         "tasks":[
+//             {"taskId":97522,"content":"Record Javascript Intro"},
+//             {"taskId":7243,"content":"Record Kanban Project"}
+//         ]
+//     },{
+//         "columnId":1,
+//         "tasks":[
+//             {"taskId":38833,"content":"System Design Notes"},
+//         ]
+//     },{
+//         "columnId":2,
+//         "tasks":[
+//             {"taskId":88198,"content":"React Projects"},
+//             {"taskId":23030,"content":"Edit OOPS Lectures"}
+//         ]
+//     }
+// ]
+
+
+
+//-------------
+
+// const DbInit = ()=>{
+//     localStorage.setItem("data",JSON.stringify(data))
+// }
+// DbInit();
+// console.log(kanban.getAllTasks());
+// console.log(kanban.getTask(0));
+// console.log(kanban.insertTask(0,"this one is also a task"));  
+// kanban.deleteTask(7);
+// kanban.updateTask(97522,{
+//     columnId:1,
+//     content:"Rec this js into 1"
+// })
+// console.log(kanban.getAllTasks());
+
+//---------------
